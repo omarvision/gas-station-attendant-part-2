cb46ade6ec651c421b3af18e4cca02613c4f99a7
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 12:00:41 -0400

    Add files via upload

---
+++ Attendant.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Attendant : MonoBehaviour
+{
+    public NavMeshAgent nma = null;
+    public Animator anim = null;
+    public GameObject WaitSpot = null;
+    public GameObject GasCap = null;
+    private GameObject currTarget = null;
+
+    private void Start()
+    {
+        GotoWait();
+    }
+    public void Update()
+    {
+        if (currTarget != null)
+        {
+            nma.SetDestination(currTarget.transform.position);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("WaitSpot") == true)
+        {
+            anim.SetBool("run", false);
+        }
+        else if (other.CompareTag("GasCap") == true)
+        {
+            anim.SetBool("run", false);
+            anim.SetTrigger("stand");
+        }
+    }
+    public void GotoWait()
+    {
+        anim.SetBool("run", true);
+        currTarget = WaitSpot;
+    }
+    public void GotoCar()
+    {
+        anim.SetBool("run", true);
+        currTarget = GasCap;
+    }
+}
---
+++ GasStation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GasStation : MonoBehaviour
+{
+    public Attendant attendant = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //gas station trigger area - is hit
+        attendant.GotoCar();
+    }
+}
